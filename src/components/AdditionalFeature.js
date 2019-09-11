66627b568e70da95a59cd2db0a94aff8b37188e5
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-11 15:14:31 -0700

    add the ability to buy additional items

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { buyItem } from '../actions'
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={() => props.buyItem(props.feature)}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+function mapDispatchToProps() {
+  return {
+    buyItem
+  }
+}
+
+export default connect(
+  null, 
+  mapDispatchToProps()
+)(AdditionalFeature);
